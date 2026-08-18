be3a4e30ff7355b4444b1b5620322f5d7f12a3e5
Hamish McIntyre <hamish@mcintyre.au>
2026-08-18 16:16:20 +1000

    Player movement updates claude: fix diagonal movement bug i found

---
+++ src/Entity/entity.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <raylib.h>
+#include <raymath.h>
+#include <rlgl.h>
+
+class Entity {
+public:
+  virtual ~Entity() = default;
+
+  virtual void Update(float dt, Camera3D &camera) = 0;
+  virtual void Draw() const {
+    Matrix matScale       = MatrixScale(transform.scale.x, transform.scale.y, transform.scale.z);
+    Matrix matRotation    = QuaternionToMatrix(transform.rotation);
+    Matrix matTranslation = MatrixTranslate(transform.translation.x, transform.translation.y, transform.translation.z);
+
+    Matrix matTransform 
+    = MatrixMultiply(MatrixMultiply(matScale, matRotation), matTranslation);
+
+    rlPushMatrix();
+    rlMultMatrixf(MatrixToFloat(matTransform));
+    DrawCube(Vector3Zero(), 1.0f, 1.0f, 1.0f, WHITE);
+    DrawCubeWires(Vector3Zero(), 1.0f, 1.0f, 1.0f, BLACK);
+    rlPopMatrix();
+  };
+
+protected:
+  Transform transform{};
+  Vector3 velocity;
+  bool onGround = true;
+};
---
+++ src/Player/player.cpp
@@ -0,0 +1,113 @@
+#include "Player/player.hpp"
+#include <raylib.h>
+#include <raymath.h>
+#include <rlgl.h>
+
+#define PLAYERCOLOR RED
+
+constexpr float GRAVITY = 140.0f;
+
+void Player::Update(float dt, Camera3D &camera) {
+  // ==== player movement ====
+  Vector3 lookForward = Vector3RotateByQuaternion({0.0f, 0.0f, -1.0f}, transform.rotation);
+
+  // Flattened, used for ground movement only
+  Vector3 moveForward = lookForward;
+  moveForward.y       = 0.0f;
+  moveForward         = Vector3Normalize(moveForward);
+
+  Vector3 right     = Vector3RotateByQuaternion({1.0f, 0.0f, 0.0f}, transform.rotation);
+  Vector3 moveRight = right;
+  moveRight.y       = 0.0f;
+  moveRight         = Vector3Normalize(moveRight);
+
+  if (transform.translation.y < transform.scale.y * 2) {
+    onGround                = true;
+    velocity.y              = 0;
+    transform.translation.y = transform.scale.y * 2;
+  }
+
+  Vector3 moveDir = Vector3Zero();
+
+  if (IsKeyDown(KEY_W))
+    moveDir = Vector3Add(moveDir, moveForward);
+  if (IsKeyDown(KEY_S))
+    moveDir = Vector3Subtract(moveDir, moveForward);
+  if (IsKeyDown(KEY_D))
+    moveDir = Vector3Add(moveDir, moveRight);
+  if (IsKeyDown(KEY_A))
+    moveDir = Vector3Subtract(moveDir, moveRight);
+
+  float multiplier = onGround ? 1.0f : 0.05f;
+  if (IsKeyDown(KEY_LEFT_SHIFT)) {
+    multiplier *= 0.2f;
+  }
+
+  if (Vector3Length(moveDir) > 0.0f) {
+    moveDir  = Vector3Normalize(moveDir); // prevents diagonal movement being faster
+    velocity = Vector3Add(velocity, Vector3Scale(moveDir, speed * multiplier));
+  }
+
+  if (onGround && IsKeyDown(KEY_SPACE)) {
+    onGround   = false;
+    velocity.y = jumpPower;
+  }
+
+  Vector2 horizontalVel = {velocity.x, velocity.z};
+  if (Vector2Length(horizontalVel) > 50.0f) {
+    horizontalVel = Vector2Scale(Vector2Normalize(horizontalVel), 50.0f);
+    velocity.x    = horizontalVel.x;
+    velocity.z    = horizontalVel.y;
+  }
+  if (onGround) {
+    float damping = powf(0.7f, dt * 60.0f);
+    velocity.x *= damping;
+    velocity.z *= damping;
+  }
+  if (!onGround) {
+    velocity.y -= GRAVITY * dt;
+    velocity.y = Clamp(velocity.y, -20.0f, jumpPower);
+  }
+
+  transform.translation = Vector3Add(transform.translation, Vector3Scale(velocity, dt));
+
+  // ==== mouse rotaton =====
+  Vector2 mouseDelta = GetMouseDelta();
+
+  float mouseSensitivity = 0.003f;
+
+  yaw -= mouseDelta.x * mouseSensitivity;
+  pitch -= mouseDelta.y * mouseSensitivity;
+
+  float pitchLimit = 89.0f * DEG2RAD;
+  pitch            = Clamp(pitch, -pitchLimit, pitchLimit);
+
+  transform.rotation = QuaternionFromEuler(pitch, yaw, 0.0f);
+
+  // ==== update camera ====
+  camera.position = transform.translation;
+  camera.target   = Vector3Add(transform.translation, lookForward);
+}
+
+Player::Player() {
+  transform.rotation    = QuaternionFromEuler(0, 0, 0);
+  transform.scale       = {0.7, 5, 0.7};
+  transform.translation = {0, 10, 0};
+
+  DisableCursor();
+}
+
+void Player::Draw() const {
+  Matrix matScale    = MatrixScale(transform.scale.x, transform.scale.y, transform.scale.z);
+  Matrix matRotation = QuaternionToMatrix(transform.rotation);
+  Matrix matTranslation =
+      MatrixTranslate(transform.translation.x, transform.translation.y, transform.translation.z);
+
+  Matrix matTransform = MatrixMultiply(MatrixMultiply(matScale, matRotation), matTranslation);
+
+  rlPushMatrix();
+  rlMultMatrixf(MatrixToFloat(matTransform));
+  DrawCube(Vector3Zero(), 1.0f, 1.0f, 1.0f, RED);
+  DrawCubeWires(Vector3Zero(), 1.0f, 1.0f, 1.0f, BLACK);
+  rlPopMatrix();
+};
