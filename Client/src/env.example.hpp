965ca6514285db10b723a1d294f8419120751548
Hamish McIntyre <hamish.oliver@icloud.com>
2026-08-30 09:16:23 +1000

    remove unnecessary env lines

--- Client/src/env.example.hpp
+++ Client/src/env.example.hpp
@@ -1,14 +1,10 @@
 
 #pragma once
 
-#ifndef SERVER_IP
 #define SERVER_IP "127.0.0.1"
-#endif
-#ifndef SERVER_PORT
 #define SERVER_PORT 9798
-#endif
 
 // #define SERVER_WSS
-// #define DEBUG
+#define DEBUG
 
 // #define CHEATS
